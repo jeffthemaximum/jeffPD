944d16582306a8bdfb67378f98c1fc3ae683a7be
Curtis Finnigan <curtisfinnigan@IALMAC.local>
2015-08-28 15:42:44 -0400

    major db updates. coaches, teachers, logs. coaches have many logs. teachers have many logs. logs have many teachers but one coach.

--- app/models.py
+++ app/models.py
@@ -24,6 +24,8 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     first_name = db.Column(db.String(64))
     last_name = db.Column(db.String(64))
+    type = db.Column(db.String(50))
+    __mapper_args__ = {'polymorphic_identity': 'users', 'polymorphic_on':type}
 
     @property
     def password(self):
@@ -55,21 +57,46 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+
 class Teacher(User):
-    logs = db.relationship('Log', backref='teacher', lazy='dynamic')
+    __tablename__ = 'teachers'
+    __mapper_args__ = {'polymorphic_identity': 'teachers'}
+    id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    # teachers have many logs
+    logs = db.relationship(
+        'Log',
+        secondary='log_teacher_link'
+    )
+
 
 class Coach(User):
+    __tablename__= 'coaches'
+    __mapper_args__ = {'polymorphic_identity': 'coach'}
+    id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    # coaches have many logs
     logs = db.relationship('Log', backref='coach', lazy='dynamic')
 
+
 class Log(db.Model):
-    __tablename__ = 'posts'
+    __tablename__ = 'logs'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    # posts have one user and one game
-    teacher_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    coach_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # logs have many teachers
+    teachers = db.relationship(
+        Teacher,
+        secondary='log_teacher_link'
+    )
+    # logs have one coach
+    coach_id = db.Column(db.Integer, db.ForeignKey('coaches.id'))
 
+
+class LogTeacherLink(db.Model):
+    __tablename__ = 'log_teacher_link'
+    log_id = db.Column(db.Integer, db.ForeignKey('logs.id'), primary_key=True)
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), primary_key=True)
+    log = db.relationship(Log, backref=db.backref("teacher_assoc"))
+    teacher = db.relationship(Teacher, backref=db.backref("log_assoc"))
 
 
 @login_manager.user_loader

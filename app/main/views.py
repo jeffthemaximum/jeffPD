e8e4d17460b7515133b61ddccf492783baa54fc9
Curtis Finnigan <curtisfinnigan@IALMAC.local>
2015-09-04 14:43:35 -0400

    9-4 gmail filters

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, url_for
 from flask.ext.login import current_user, login_required
 from . import main
 
@@ -12,9 +12,20 @@ def index():
 
 @main.route('/pd_list')
 def pd_list():
+    if current_user.is_authenticated() != True:
+        flash("Unlock super cool extras features by logging in or registering. You can do this by clicking 'Login' in the header!")
     return render_template('pd_list.html')
 
-@main.route('/pd_list/2015_8_27_first_day_google')
+@main.route('/pd_content/2015_8_27_first_day_google')
 # @login_required .. no log in required anymore, not just a flash with a recommendation to sign in.
 def pd_one():
+    if current_user.is_authenticated() != True:
+        flash("Unlock super cool extras features by logging in or registering. You can do this by clicking 'Login' in the header!")
     return render_template('pd_content/2015_8_27_first_day_google.html')
+
+
+@main.route('/pd_content/2015_9_4_gmail_filters.html')
+def pd_two():
+    if current_user.is_authenticated() != True:
+        flash("Unlock super cool extras features by logging in or registering. You can do this by clicking 'Login' in the header!")
+    return render_template('/pd_content/2015_9_4_gmail_filters.html')

c23abb4a825a1ca603f4b4df4493708ea482ec41
jeffthemaximum <frey.maxim@gmail.com>
2015-09-28 11:22:28 -0400

    updated with completeness search param

--- app/private/forms.py
+++ app/private/forms.py
@@ -1,5 +1,6 @@
 from flask.ext.wtf import Form
-from wtforms import SelectMultipleField, SubmitField, widgets, TextAreaField, SelectField, BooleanField
+from wtforms import SelectMultipleField, SubmitField, widgets, TextAreaField
+from wtforms import SelectField, BooleanField
 from wtforms.validators import Required
 from ..models import Teacher, Coach, Tag
 from flask.ext.login import current_user
@@ -27,7 +28,8 @@ class AddTeachersForm(Form):
         super(AddTeachersForm, self).__init__(*args, **kwargs)
         # populates the role select field choices
         # returns a list of tuples with roleid, name
-        query = Teacher.query.filter_by(school=current_user.school).order_by(Teacher.email).all()
+        query = Teacher.query.filter_by(
+            school=current_user.school).order_by(Teacher.email).all()
         choices = [(teacher.id, teacher.email) for teacher in query]
         self.teachers.choices = choices
 
@@ -38,7 +40,8 @@ class CoachLogForm(Form):
         coerce=int)
     body = TextAreaField('What\'d you do?')
     next = TextAreaField('Whatchu gonna do next?')
-    completed = BooleanField('Check if coaching is completed. Leave blank if still in progress.')
+    completed = BooleanField(
+        'Check if coaching is completed. Leave blank if still in progress.')
     tags = SelectMultipleField(
         'Tags',
         coerce=int)
@@ -52,13 +55,19 @@ class CoachLogForm(Form):
         teachers = curr_coach.teachers
         teachers = sorted(teachers, key=lambda teacher: teacher.email)
         tags = Tag.query.all()
-        self.teachers.choices = [(teacher.id, teacher.email) for teacher in teachers]
+        query_params = [(teacher.id, teacher.email) for teacher in teachers]
+        self.teachers.choices = query_params
         self.tags.choices = [(tag.id, tag.name) for tag in tags]
 
 
 class CoachSelectsTags(Form):
     tags = SelectField(
         'Search by Tag. Leave blank to view all logs.',
+        coerce=int,
+        default=(0, ''))
+    completed = SelectField(
+        u'Search by complete. Leave blank to view all logs.',
+        choices=[('0', ''), ('1', 'Done'), ('2', 'In Progress')],
         coerce=int,
         default=(0, ''))
     submit = SubmitField('Submit')
@@ -85,7 +94,8 @@ class AdministratorSelectsCoachesForm(Form):
         super(AdministratorSelectsCoachesForm, self).__init__(*args, **kwargs)
         # populates the role select field choices
         # returns a list of tuples with roleid, name
-        coaches = Coach.query.filter_by(school=current_user.school).order_by(Teacher.email).all()
+        coaches = Coach.query.filter_by(
+            school=current_user.school).order_by(Teacher.email).all()
         self.coach.choices = [(coach.id, coach.email) for coach in coaches]
         tags = Tag.query.order_by(Tag.id).all()
         self.tags.choices = [(tag.id, tag.name) for tag in tags]
@@ -102,5 +112,7 @@ class AdministratorSelectsTeachersForm(Form):
         super(AdministratorSelectsTeachersForm, self).__init__(*args, **kwargs)
         # populates the role select field choices
         # returns a list of tuples with roleid, name
-        teachers = Teacher.query.filter_by(school=current_user.school).order_by(Teacher.email).all()
-        self.teacher.choices = [(teacher.id, teacher.email) for teacher in teachers]
+        teachers = Teacher.query.filter_by(
+            school=current_user.school).order_by(Teacher.email).all()
+        query_params = [(teacher.id, teacher.email) for teacher in teachers]
+        self.teacher.choices = query_params
